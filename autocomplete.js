83874754475bbeb13ef036372aa8c5a30ebe9a63
nadia <moradi.nadiw@gmail.com>
2026-06-14 11:14:18 +0330

    Renaming movie references

--- autocomplete.js
+++ autocomplete.js
@@ -1,6 +1,12 @@
-const createAutoComplete = ({root , renderOption,onOptionSelect , inputValue}) => {
+const createAutoComplete = ({
+    root,
+    renderOption,
+    onOptionSelect,
+    inputValue,
+    fetchData
+}) => {
 root.innerHTML = `
-  <label class="mb-2 block font-bold text-gray-800">Search For a Movie</label>
+  <label class="mb-2 block font-bold text-gray-800">Search</label>
   <input class="w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 shadow-sm outline-none transition focus:border-teal-500 focus:ring-2 focus:ring-teal-200" />
   <div class="dropdown relative hidden w-full">
     <div class="absolute left-0 right-0 z-10 mt-1 overflow-hidden rounded-md border border-gray-200 bg-white shadow-lg">
@@ -20,26 +26,24 @@ const onInput = (event) => {
   }
 
   timeoutId = setTimeout(async () => {
-    const movies = await fetchData(event.target.value);
+    const items = await fetchData(event.target.value);
 
-    if(!movies.length){
+    if(!items.length){
       dropdown.classList.add('hidden')
       return
     }
     resultsWrapper.innerHTML = '';
     dropdown.classList.remove('hidden');
 
-for (let movie of movies) {
-  const option = document.createElement('a');
-
-
-  option.className =
+for (let item of items) {
+    const option = document.createElement('a');
+    option.className =
     'flex h-[60px] cursor-pointer items-center gap-3 px-4 py-2 text-gray-800 hover:bg-gray-100';
-    option.innerHTML = renderOption(movie)
+    option.innerHTML = renderOption(item)
     option.addEventListener('click', () => {
     dropdown.classList.add('hidden');
-    input.value = inputValue(movie);
-    onOptionSelect(movie)
+    input.value = inputValue(item);
+    onOptionSelect(item)
   });
 
   resultsWrapper.appendChild(option);
